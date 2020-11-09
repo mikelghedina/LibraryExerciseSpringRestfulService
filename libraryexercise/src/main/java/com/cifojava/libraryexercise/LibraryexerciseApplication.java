60f739eedd350c0b1fecfbb35f72a44924b07b6f
mikelghedina <mikel.ghedina@gmail.com>
2020-11-09 12:03:43 +0100

    Initial commit

---
+++ libraryexercise/src/main/java/com/cifojava/libraryexercise/LibraryexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.cifojava.libraryexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryexerciseApplication.class, args);
+	}
+
+}
---
+++ libraryexercise/src/test/java/com/cifojava/libraryexercise/LibraryexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cifojava.libraryexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
