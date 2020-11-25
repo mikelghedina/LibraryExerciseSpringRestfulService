b467e7be766ab5b181ae354a19ac8b96ecde64ff
mikelghedina <mikel.ghedina@gmail.com>
2020-11-25 13:43:19 +0100

    Class updates to project.

--- libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/RegistryRestController.java
+++ libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/RegistryRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class RegistryRestController {
 
