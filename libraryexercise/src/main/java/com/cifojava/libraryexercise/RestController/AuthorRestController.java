5c5c95f29dd7ad3700f41bb4040a7a77aa57d9ee
mikelghedina <mikel.ghedina@gmail.com>
2020-11-25 13:44:41 +0100

    Class updates to project.

--- libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/AuthorRestController.java
+++ libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/AuthorRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class AuthorRestController {
 
--- libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/BookRestController.java
+++ libraryexercise/src/main/java/com/cifojava/libraryexercise/RestController/BookRestController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class BookRestController {
 
